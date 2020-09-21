bfffb6612d040dc619cdd1a65cc4b71c48d876d7
Sandesh Shrestha <sandeshshrestha45@gmail.com>
2020-09-21 12:40:13 +0545

    Update Nepali_NewsRecommendationEngine.py

--- Nepali_NewsRecommendationEngine.py
+++ Nepali_NewsRecommendationEngine.py
@@ -71,7 +71,7 @@ def main():
     st.title("EI Maven")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">News Recommendation Engine (English) </h2>
+    <h2 style="color:white;text-align:center;">News Recommendation Engine (Nepali) </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
